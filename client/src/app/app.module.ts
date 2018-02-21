a2d75b53a2eede027f6dea3d261d41b3b24c5baf
Lorentz Vedeler <lorentz.vedeler@bouvet.no>
2018-02-21 16:10:16 +0100

    add navigation

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,19 +8,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PathNotFoundComponent } from './path-not-found/path-not-found.component';
 import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
+import { SharedModule } from './shared/shared.module';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     PathNotFoundComponent,
-    HomeComponent
+    HomeComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule, MatListModule } from '@angular/material'
+import { MatButtonModule, MatListModule, MatToolbarModule } from '@angular/material'
 
 @NgModule({
   imports: [
     CommonModule,
     MatButtonModule,
-    MatListModule
+    MatListModule,
+    MatToolbarModule
   ],
   exports:[
     MatButtonModule,
-    MatListModule
+    MatListModule,
+    MatToolbarModule
   ],
   declarations: []
 })
